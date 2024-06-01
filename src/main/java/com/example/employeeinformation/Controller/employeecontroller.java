16c87b0615184b8fa180bb6bfe62bfa6af28b906
sujji1729 <sujathanu96@gmail.com>
2024-06-01 16:29:13 +0530

    controller and properties update

--- src/main/java/com/example/employeeinformation/Controller/employeecontroller.java
+++ src/main/java/com/example/employeeinformation/Controller/employeecontroller.java
@@ -6,114 +6,108 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import com.example.employeeinformation.Entity.employeeTable;
 import com.example.employeeinformation.Repository.employeerepo;
 
 @Controller
 public class employeecontroller {
-	
+
 	@Autowired
 	employeerepo employeerepoo;
-	
+
 	@GetMapping(value = "/userform")
-	public String userform() { 
-		return "userform"; 
+	public String userform() {
+		return "userform";
 	}
 
-	
 	@PostMapping(value = "/newEmployeeDetails")
 	public String addEmployeeDetails(@ModelAttribute employeeTable employeeData, Model model) {
-		
-		  employeerepoo.save(employeeData);
-		  model.addAttribute("allEmployees", employeerepoo.findAll());
-		  return "homepage";
+
+		employeerepoo.save(employeeData);
+		model.addAttribute("allEmployees", employeerepoo.findAll());
+		return "homepage";
 	}
-	
-	 
-	@GetMapping(value = "/homepage") 
-	public String getAllEmployees(Model model){
-		
+
+	@GetMapping(value = "/homepage")
+	public String getAllEmployees(Model model) {
+
 		List<employeeTable> allEmployees = employeerepoo.findAll();
 		model.addAttribute("allEmployees", allEmployees);
-			
-		return "homePage";	
+
+		return "homePage";
 	}
-	
-	//show create user html page
+
+	// show create user html page
 	@GetMapping(value = "/createuser")
-	public String createpage(){
-			
-		return "userform";	
+	public String createpage() {
+
+		return "userform";
 	}
-	
-	//update page html
+
+	// update page html
 	@PostMapping(value = "/updateuser/{id}")
-	public String updatepage(@PathVariable long id, Model model){
+	public String updatepage(@PathVariable long id, Model model) {
 		Optional<employeeTable> findId = employeerepoo.findById(id);
-		
-		 model.addAttribute("allEmployees", findId.get());
-		return "updateuserform";	
+
+		model.addAttribute("allEmployees", findId.get());
+		return "updateuserform";
 	}
-	
-	// individual user 
-	
+
+	// individual user
+
 	@GetMapping(value = "/getOneEmployee/{id}")
-	public employeeTable getOneEmployee(@PathVariable long id){
+	public employeeTable getOneEmployee(@PathVariable long id) {
 		Optional<employeeTable> findId = employeerepoo.findById(id);
 		if (findId.isPresent()) {
 			return findId.get();
-		}
-		else {
+		} else {
 			return null;
 		}
-		
+
 	}
-	
-	//update
+
+	// update
 	@PostMapping(value = "/updateEmployeeDetails/{id}")
-	public String updateEmployeeDetails(@PathVariable long id, @ModelAttribute employeeTable employeeData, Model model) {
-	
-	Optional<employeeTable> findId = employeerepoo.findById(id);
-	
-	if (findId.isPresent()) {
-		
-		findId.get().setId(employeeData.getId());
-		findId.get().setFirstName(employeeData.getFirstName());
-		findId.get().setLastName(employeeData.getLastName());
-		findId.get().setEmail(employeeData.getEmail());
-		findId.get().setAddress(employeeData.getAddress());
-		
-		employeerepoo.save(findId.get());
-		 model.addAttribute("allEmployees", employeerepoo.findAll());
-		  return "homepage";
+	public String updateEmployeeDetails(@PathVariable long id, @ModelAttribute employeeTable employeeData,
+			Model model) {
+
+		Optional<employeeTable> findId = employeerepoo.findById(id);
+
+		if (findId.isPresent()) {
+
+			findId.get().setId(employeeData.getId());
+			findId.get().setFirstName(employeeData.getFirstName());
+			findId.get().setLastName(employeeData.getLastName());
+			findId.get().setEmail(employeeData.getEmail());
+			findId.get().setAddress(employeeData.getAddress());
+
+			employeerepoo.save(findId.get());
+			model.addAttribute("allEmployees", employeerepoo.findAll());
+			return "homepage";
+		} else {
+			return null;
+		}
+
 	}
-	else {
-		return null;
-	}
-	
-	}
-	
-	//delete
+
+	// delete
 	@PostMapping(value = "/deleteEmployeeDetails/{id}")
 	public String deleteEmployeeDetails(@PathVariable long id, Model model) {
 		Optional<employeeTable> findId = employeerepoo.findById(id);
-		
+
 		if (findId.isPresent()) {
 			employeerepoo.deleteById(id);
 			model.addAttribute("allEmployees", employeerepoo.findAll());
 			return "homePage";
-		}
-		else {
+		} else {
 			return "homePage";
 		}
-	}	
+	}
 
 }
